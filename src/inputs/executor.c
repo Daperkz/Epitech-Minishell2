e674c67b9c666f5224328eb09f2635232279665e
August Conchin <august.conchin@epitech.eu>
2026-03-11 15:13:45 +0100

    feat: modularity
    	- add a new under function the actual executing part
    	- old execute_command works as a wrapper to allow for external forks (typically for pipes)

--- src/inputs/executor.c
+++ src/inputs/executor.c
@@ -28,6 +28,14 @@ static void error_printing(char *cmd)
     }
 }
 
+int execute_child(shell_t *shell, char *program_path)
+{
+    signal(SIGINT, SIG_DFL);
+    execve(program_path, shell->input_array, shell->env);
+    error_printing(shell->input_array[0]);
+    exit(EXIT_FAILURE);
+}
+
 int execute_command(shell_t *shell, char *program_path)
 {
     shell->child = fork();
@@ -36,10 +44,7 @@ int execute_command(shell_t *shell, char *program_path)
         return (EXIT_FAILURE);
     }
     if (shell->child == 0) {
-        signal(SIGINT, SIG_DFL);
-        execve(program_path, shell->input_array, shell->env);
-        error_printing(shell->input_array[0]);
-        exit(EXIT_FAILURE);
+        execute_child(shell, program_path);
     } else {
         waitpid(shell->child, &shell->child_status, WUNTRACED);
         handle_child_status(shell, shell->child_status);
