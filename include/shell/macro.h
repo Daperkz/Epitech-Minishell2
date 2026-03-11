43997539406c12fd44d6b1b214afc38d73816cb4
August Conchin <august.conchin@epitech.eu>
2026-03-11 16:50:46 +0100

    feat: add new macro.h file to seperate correctly content of header

---
+++ include/shell/macro.h
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2026
+** macro.h
+** File description:
+** macro
+*/
+
+#ifndef SHELL_MACRO_H_
+    #define SHELL_MACRO_H_
+
+    #define MALLOC_FAIL (void *)-1
+
+    #define EXIT_SUCCESS 0
+    #define EXIT_FAILURE 1
+    #define EXIT_ERROR 84
+    #define EXIT_ACTION_DONE 2
+    #define EXIT_SHUTDOWN 3
+
+    #define ENV_VAR_PWD "PWD="
+    #define ENV_VAR_PWD_SIZE 4
+    #define ENV_VAR_OLDPWD "OLDPWD="
+    #define ENV_VAR_OLDPWD_SIZE 7
+    #define ENV_VAR_PATH "PATH="
+    #define ENV_VAR_PATH_SIZE 5
+    #define ENV_VAR_ZDOTDIR "ZDOTDIR="
+    #define ENV_VAR_ZDOTDIR_SIZE 8
+
+    #define ZSH_NOERR_COLOR "\033[38;5;82m"
+    #define ZSH_ERR_COLOR "\033[38;5;160m"
+    #define ZSH_CWD_COLOR "\033[1;36m"
+    #define COLOR_RESET "\033[0m"
+
+    #define THE_PROMPT "$ "
+    #define THE_PROMPT_SIZE 2
+
+    #define COMMAND_SEPERATOR ";"
+    #define PIPE_SEPERATOR "|"
+
+#endif /* !SHELL_MACRO_H_ */
