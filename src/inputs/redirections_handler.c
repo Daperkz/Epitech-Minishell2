a723e1de9cee227c8738273f8fcefc775b44376d
August Conchin <august.conchin@epitech.eu>
2026-03-12 18:37:51 +0100

    feat: redirections
    	- add the 3 basic redirections ('<', '>', '>>')
    	- chore: support the last redirections also called heredoc

--- src/inputs/redirections_handler.c
+++ src/inputs/redirections_handler.c
@@ -8,6 +8,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 #include "my/printf.h"
 #include "my/string.h"
@@ -32,18 +35,38 @@ static int is_valid(char *str)
         my_strcmp(str, "<") == 0 || my_strcmp(str, "<<") == 0);
 }
 
-static int error(shell_t *shell)
+static int validity_error(shell_t *shell)
 {
     shell->last_errno = 1;
     my_fprintf(STDERR, "Missing name for redirect\n");
     return (EXIT_ACTION_DONE);
 }
 
+static int file_error(shell_t *shell, char *file)
+{
+    shell->last_errno = errno;
+    my_fprintf(STDERR, "%s: %s.\n", file, strerror(shell->last_errno));
+    return (EXIT_ACTION_DONE);
+}
+
 static int redirect(shell_t *shell, int i)
 {
-    if (!is_valid(shell->input_array[i + 1])) {
-        return error(shell);
-    }
+    char *type = shell->input_array[i];
+    char *file = shell->input_array[i + 1];
+    int fd = -1;
+
+    if (!is_valid(file))
+        return validity_error(shell);
+    if (my_strcmp(type, ">") == 0)
+        fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    else if (my_strcmp(type, ">>") == 0)
+        fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    else
+        fd = open(file, O_RDONLY);
+    if (fd == -1)
+        return file_error(shell, file);
+    dup2(fd, (type[0] == '<') ? STDIN : STDOUT);
+    close(fd);
     shift_array(shell, i);
     return (EXIT_SUCCESS);
 }
@@ -51,7 +74,7 @@ static int redirect(shell_t *shell, int i)
 static int heredoc(shell_t *shell, int i)
 {
     if (!is_valid(shell->input_array[i + 1])) {
-        return error(shell);
+        return validity_error(shell);
     }
     shift_array(shell, i);
     return (EXIT_SUCCESS);
